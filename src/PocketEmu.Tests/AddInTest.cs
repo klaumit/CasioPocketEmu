2df93500132275df42a118aa115c3562efdf8be2
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-23 18:59:44 +0200

    Adding sample apps

--- src/PocketEmu.Tests/AddInTest.cs
+++ src/PocketEmu.Tests/AddInTest.cs
@@ -9,9 +9,19 @@ namespace PocketEmu.Tests
         [Theory]
         [InlineData("GetOS2_1.2")]
         [InlineData("GetOS_1.2")]
+        [InlineData("Puzzle_1.0")]
+        [InlineData("Sample_1.0")]
         public void ShouldReadZ486(string fileName)
         {
             var dir = Path.Combine("Resources", "Addins", "Z486");
+            DoShouldRead(dir, fileName);
+        }
+
+        [Theory]
+        [InlineData("Puzzle_1.0")]
+        public void ShouldReadZ488(string fileName)
+        {
+            var dir = Path.Combine("Resources", "Addins", "Z488");
             DoShouldRead(dir, fileName);
         }
     }
