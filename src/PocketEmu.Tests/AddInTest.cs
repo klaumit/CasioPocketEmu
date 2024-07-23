5b5570527e4e0e54fee7f49d8e06b8fb7fa27efd
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-23 19:08:13 +0200

    Adding apps

--- src/PocketEmu.Tests/AddInTest.cs
+++ src/PocketEmu.Tests/AddInTest.cs
@@ -18,7 +18,12 @@ namespace PocketEmu.Tests
         }
 
         [Theory]
+        [InlineData("Dumper_1.07")]
+        [InlineData("jgirls3_1.0")]
+        [InlineData("Mystrpg_1.0")]
         [InlineData("Puzzle_1.0")]
+        [InlineData("PVMerlin4_4.01")]
+        [InlineData("Sample_1.0")]
         public void ShouldReadZ488(string fileName)
         {
             var dir = Path.Combine("Resources", "Addins", "Z488");
