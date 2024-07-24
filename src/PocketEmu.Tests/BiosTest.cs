5afac87df665d30066726069b2c32bdc409eb2f7
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-24 23:38:43 +0200

    Adding files

--- src/PocketEmu.Tests/BiosTest.cs
+++ src/PocketEmu.Tests/BiosTest.cs
@@ -8,12 +8,16 @@ namespace PocketEmu.Tests
     {
         [Theory]
         [InlineData("G500", "BIOS500")]
+        [InlineData("G500", "BIOS_sim")]
         [InlineData("G501", "BIOS400")]
         [InlineData("G501", "BIOS600")]
+        [InlineData("G501", "BIOS_sim")]
         [InlineData("Z481", "BIOS450")]
+        [InlineData("Z481", "BIOS_sim")]
         [InlineData("Z486", "BIOS250X")]
         [InlineData("Z486", "BIOS450X")]
         [InlineData("Z488", "BIOS750")]
+        [InlineData("Z488", "BIOS_sim")]
         public void ShouldRead(string model, string fileName)
         {
             var dir = Path.Combine("Resources", "Bios", model);
