926c9828ff81496a9868bf1ce8c83167c3c66645
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-09-23 22:46:39 +0200

    Adding some files

--- src/PocketEmu.Tests/DumpTest.cs
+++ src/PocketEmu.Tests/DumpTest.cs
@@ -7,6 +7,8 @@ namespace PocketEmu.Tests
     public class DumpTest
     {
         [Theory]
+        [InlineData("PVosUA_S400")]
+        [InlineData("PVS400P")]
         [InlineData("sscxp_400")]
         [InlineData("sscxp_600")]
         public void ShouldReadG501(string fileName)
@@ -18,6 +20,12 @@ namespace PocketEmu.Tests
         [Theory]
         [InlineData("sscxp_460")]
         [InlineData("Ultra_Sx60")]
+        [InlineData("belka")]
+        [InlineData("pvos_plus_60")]
+        [InlineData("PVosUA_S460")]
+        [InlineData("pvos_x60_orig")]
+        [InlineData("pvos_x60_ua")]
+        [InlineData("ssc_pvos_mod_x60")]
         public void ShouldReadG500(string fileName)
         {
             var dir = Path.Combine("Resources", "Dumps", "G500");
@@ -29,6 +37,19 @@ namespace PocketEmu.Tests
         [InlineData("s250")]
         [InlineData("ssc_pvos")]
         [InlineData("sscxp_250")]
+        [InlineData("PVosUA_S250")]
+        [InlineData("pvs450r")]
+        [InlineData("pvs_kbl")]
+        [InlineData("pvsrus")]
+        [InlineData("s450_apps_eng")]
+        [InlineData("s450_menu_eng3")]
+        [InlineData("s450_menu_apps_eng")]
+        [InlineData("s450_menu_eng")]
+        [InlineData("ssc_apps")]
+        [InlineData("ssc_menu3")]
+        [InlineData("ssc_menu_apps3")]
+        [InlineData("ssc_menu")]
+        [InlineData("ssc_menu_apps")]
         public void ShouldReadZ481(string fileName)
         {
             var dir = Path.Combine("Resources", "Dumps", "Z481");
@@ -45,6 +66,9 @@ namespace PocketEmu.Tests
         [InlineData("sscxp_250x")]
         [InlineData("sscxp_450x")]
         [InlineData("pv450xrus")]
+        [InlineData("pv250xrus2")]
+        [InlineData("pv450xrus3")]
+        [InlineData("PVosUA_450X")]
         public void ShouldReadZ486(string fileName)
         {
             var dir = Path.Combine("Resources", "Dumps", "Z486");
@@ -55,6 +79,7 @@ namespace PocketEmu.Tests
         [InlineData("750plus")]
         [InlineData("750up120")]
         [InlineData("sscxp_750")]
+        [InlineData("PVosUA_750")]
         public void ShouldReadZ488(string fileName)
         {
             var dir = Path.Combine("Resources", "Dumps", "Z488");
